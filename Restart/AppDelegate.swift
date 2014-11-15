1db952e6e1bdf8ea7bd61ad2110264a00423fa59
Sam Soffes <sam@soff.es>
2014-11-15 10:10:32 -0800

    Restart

---
+++ Restart/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  Restart
+//
+//  Created by Sam Soffes on 11/15/14.
+//  Copyright (c) 2014 Nothing Magical Inc. All rights reserved.
+//
+
+import Cocoa
+import UtilityKit
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+	func applicationDidFinishLaunching(aNotification: NSNotification) {
+		UtilityKit.sendEvent(.Restart)
+		NSApplication.sharedApplication().terminate(nil)
+	}
+}
